309da0df180e8ecb2b4aa5ee6219623d83493e7a
S0526 <S0526@19S0526>
2023-11-09 14:12:11 +0900

    初コミット

---
+++ EbiProject/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+@Controller
+public class LoginController {
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public String login(String yuza,int NUMBER, int PASS,RedirectAttributes redirectAttributes) throws IOException {
+		if ((NUMBER==2201085)&&(PASS==1920)) {
+			return "redirect:/top";
+		} else {
+			redirectAttributes.addFlashAttribute("yuza", yuza);
+			redirectAttributes.addFlashAttribute("NUMBER", NUMBER);
+			redirectAttributes.addFlashAttribute("PASS", PASS);
+			return "redirect:/login";
+
+		}
+	}}
