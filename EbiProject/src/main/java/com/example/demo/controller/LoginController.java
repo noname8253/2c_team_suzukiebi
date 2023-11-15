051af785e898e22e4f53ceed197a57f791809d7f
S0508 <S0508@19S0508>
2023-11-15 10:22:52 +0900

    ログイン画面3回目

--- EbiProject/src/main/java/com/example/demo/controller/LoginController.java
+++ EbiProject/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,29 +1,22 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Controller
+@RequestMapping("/login")
 public class LoginController {
-	//画面表示用
-	@RequestMapping(path = "/login", method = RequestMethod.GET)
-	public String login() {
-		return "login";
-	}
+    //ログイン画面への遷移
+    @GetMapping
+    String getLogin() {
+        return "login";
+    }
 
-	//ログイン検証用
-	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public String login(String yuza,int NUMBER, int PASS,RedirectAttributes redirectAttributes) throws IOException {
-		if ((NUMBER==2201085)&&(PASS==1920)) {
-			return "redirect:/top";
-		} else {
-			redirectAttributes.addFlashAttribute("yuza", yuza);
-			redirectAttributes.addFlashAttribute("NUMBER", NUMBER);
-			redirectAttributes.addFlashAttribute("PASS", PASS);
-			return "redirect:/login";
-
-		}
-	}}
+    //ログイン成功時のメニュー画面への遷移
+    @PostMapping
+    String postLogin() {
+        return "top";
+    }
+}
