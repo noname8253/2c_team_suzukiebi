d9268476a3c46fc3699a8edb350c1d97d531bae2
S0526 <S0526@19S0526>
2023-11-24 16:15:03 +0900

    11/24鈴木1回目

--- EbiProject/src/main/java/com/example/demo/controller/javakiso/JavaKiso_TopController.java
+++ EbiProject/src/main/java/com/example/demo/controller/javakiso/JavaKiso_TopController.java
@@ -1,28 +1,13 @@
 package com.example.demo.controller.javakiso;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JavaKiso_TopController {
 
-	@Autowired
-	JdbcTemplate jdbcTemplate;
-	
-	@GetMapping("javakiso/javakiso_top")
-	public String javakiso_top(Model model) {
-		
-		List<Map<String, Object>> resultList;
-		String resultlist = ("");
-		resultList = jdbcTemplate.queryForList("select 科目名 from 科目");
-		model.addAttribute(resultlist,resultList);
-		
+	@GetMapping("/javakiso/javakiso_top")
+	public String subject() {
 		return "javakiso/javakiso_top";
 	}
 
