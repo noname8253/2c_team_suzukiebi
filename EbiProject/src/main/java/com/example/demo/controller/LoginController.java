762bfcb8524775ae0d0401e6a7eea6994569c93c
S0508 <S0508@19S0508>
2023-11-24 16:19:49 +0900

    11/24エビ2回目

--- EbiProject/src/main/java/com/example/demo/controller/LoginController.java
+++ EbiProject/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,39 +1,22 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
     //ログイン画面への遷移
-	@RequestMapping(path = "/login", method = RequestMethod.GET)
-    public String getLogin() {
+    @GetMapping
+    String getLogin() {
         return "login";
-        }
-    
+    }
 
     //ログイン成功時のメニュー画面への遷移
-<<<<<<< HEAD
     @PostMapping
     String top() {
         return "top";
     }
 }
-=======
-	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public String login(String login-name ,String login-pass,RedirectAttributes redirectAttributes) throws IOException {
-		if (login-name.equals("2201085") && login-pass.equals("1920")) {
-			return "/top";
-		} else {
-			redirectAttributes.addFlashAttribute("yuza", yuza);
-			redirectAttributes.addFlashAttribute("ID", ID);
-			redirectAttributes.addFlashAttribute("PASS", PASS);
-			return "redirect:/ng";
-
-		}
-	}
->>>>>>> branch 'master' of https://github.com/noname8253/2c_team_suzukiebi.git
