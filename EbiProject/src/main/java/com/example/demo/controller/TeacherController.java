7a16af95b366b507449d426dad2ae48f7f7aaed7
S0508 <S0508@19S0508>
2024-01-24 10:07:04 +0900

    エビ1/24

--- EbiProject/src/main/java/com/example/demo/controller/TeacherController.java
+++ EbiProject/src/main/java/com/example/demo/controller/TeacherController.java
@@ -1,14 +1,25 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TeacherController {
 
-	@GetMapping("/teacher")
-	public String teacher() {
-		return "teacher";
-	}
-
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public String displayTeacher(Model model) {
+        List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT * FROM teacher");
+            model.addAttribute("selectResult", resultList);
+            
+            return "/teacher";
+        }
 }
